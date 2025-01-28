dd57e031e4ea5020e5710c0248ea9dfce85f05d1
Terer <terermike3@gmail.com>
2025-01-28 22:26:18 +0300

    Styled to show active tab

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import NavLink from "./nav-link";
 import MainHeaderBackground from "./main-header-background";
 export default function MainHeader() {
   return (
@@ -17,8 +18,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href={"/meals"}>Browse Meals</Link>
-              <Link href={"/community"}>Foodies Comminity</Link>
+              <NavLink href={"/meals"}>Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href={"/community"}>Foodies Comminity</NavLink>
             </li>
           </ul>
         </nav>
